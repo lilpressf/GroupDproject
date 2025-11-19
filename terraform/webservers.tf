2f9ae4e6456b1bb240e4dc418661a6c0fe458860
Pieter Naucke <554603@student.fontys.nl>
2025-11-19 10:51:13 +0100

    added webservers. deleted instances.tf (agreed with fares)

---
+++ terraform/webservers.tf
@@ -0,0 +1,33 @@
+locals {
+  user_data = <<-EOT
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras enable nginx1
+    yum install -y nginx mysql wget tar
+
+    systemctl start nginx
+    systemctl enable nginx
+  EOT
+}
+# aws_security_group moet nog aangepast worden
+resource "aws_instance" "web1" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sub_private_1
+  private_ip             = "10.0.5.1"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "Project1"
+  user_data              = local.user_data
+  tags = { Name = "lab-g1-web-easy-01" }
+}
+
+resource "aws_instance" "web2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.sub_private_2
+  private_ip             = "10.0.6.1"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = "Project1"
+  user_data              = local.user_data
+  tags = { Name = "lab-g1-web-easy-02" }
+}
