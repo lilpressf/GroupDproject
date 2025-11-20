d554b10d0584d7309cbd155e63a9848757282134
lilpressf <daanwelten05@gmail.com>
2025-11-20 14:20:17 +0100

    internetgateway issue with two rt tables refering to same subnet

--- terraform/internetgateway.tf
+++ terraform/internetgateway.tf
@@ -21,20 +21,6 @@ resource "aws_route_table_association" "public_2_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.vpc_narre_main.id
-}
-
-resource "aws_route_table_association" "private_1_assoc" {
-  subnet_id      = aws_subnet.sub_private_1.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private_2_assoc" {
-  subnet_id      = aws_subnet.sub_private_2.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
 # Private Route Table For NAT Gateway
 
 resource "aws_route_table" "private_rt_nat" {
