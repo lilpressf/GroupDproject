2ed88b7b598502486e975cb4385991fa2da948a8
lilpressf <daanwelten05@gmail.com>
2025-11-20 14:12:03 +0100

    Added nat gateway

--- terraform/internetgateway.tf
+++ terraform/internetgateway.tf
@@ -34,3 +34,28 @@ resource "aws_route_table_association" "private_2_assoc" {
   subnet_id      = aws_subnet.sub_private_2.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+# Private Route Table For NAT Gateway
+
+resource "aws_route_table" "private_rt_nat" {
+  vpc_id = aws_vpc.vpc_narre_main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt-nat"
+  }
+}
+
+resource "aws_route_table_association" "private_1_assoc_nat" {
+  subnet_id      = aws_subnet.sub_private_1.id
+  route_table_id = aws_route_table.private_rt_nat.id
+}
+
+resource "aws_route_table_association" "private_2_assoc_nat" {
+  subnet_id      = aws_subnet.sub_private_2.id
+  route_table_id = aws_route_table.private_rt_nat.id
+}
---
+++ terraform/natgateway.tf
@@ -0,0 +1,22 @@
+# NAT Gateway
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name    = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.sub_public_1.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [
+    aws_internet_gateway.narre-igw
+  ]
+}
+
