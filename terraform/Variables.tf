06a95b555344ba1cd3b0d55520ab1427bbde6b6e
F oueslati <f.oueslati@student.fontys.nl>
2025-11-26 11:14:26 +0100

    added RDS with credentials through github actions

--- terraform/Variables.tf
+++ terraform/Variables.tf
@@ -30,3 +30,16 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
+
+#RDS variables 
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
--- terraform/database.tf
+++ terraform/database.tf
@@ -1,1 +1,19 @@
-#Fares
+resource "aws_db_instance" "mydb" {
+  identifier             = "narre-db"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  name                   = "narre_db"
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.database_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  multi_az               = false
+
+  tags = {
+    Name = "narre-db"
+  }
+}
