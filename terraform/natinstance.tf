e92fad2797b60a5359a531451989a56d92fc06e9
lilpressf <daanwelten05@gmail.com>
2025-12-17 09:52:29 +0100

    changed to t3 on nat instance

--- terraform/natinstance.tf
+++ terraform/natinstance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "nat" {
   ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.sub_public_1.id
   vpc_security_group_ids      = [aws_security_group.nat_sg.id]
   associate_public_ip_address = true
