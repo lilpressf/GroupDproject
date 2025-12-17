3083adc106f0b9d3f9de841f866ee0b8e2df0581
F oueslati <f.oueslati@student.fontys.nl>
2025-12-17 14:37:27 +0100

    fixed Database Subnet Assignment

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,17 +1,33 @@
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name = "narre-db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.sub_database_2.id,
+    aws_subnet.sub_private_1.id
+  ]
+
+  tags = {
+    Name = "narre-db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "mydb" {
-  identifier             = "narre-db"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 20
-  name                   = "narre_db"
-  username               = var.db_username
-  password               = var.db_password
+  identifier           = "narre-db"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+
+  db_name  = "narre_db"
+  username = var.db_username
+  password = var.db_password
+
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.database_sg.id]
-  skip_final_snapshot    = true
-  publicly_accessible    = false
-  multi_az               = false
+
+  publicly_accessible = false
+  multi_az            = false
+  skip_final_snapshot = true
 
   tags = {
     Name = "narre-db"
@@ -19,7 +35,6 @@ resource "aws_db_instance" "mydb" {
 }
 
 resource "null_resource" "db_init" {
-  # Only rerun if the DB instance changes or the schema file changes
   triggers = {
     db_instance_id = aws_db_instance.mydb.id
     schema_hash    = filesha1("${path.module}/db/schema.sql")
@@ -32,7 +47,7 @@ resource "null_resource" "db_init" {
         --port='${aws_db_instance.mydb.port}' \
         --user='${var.db_username}' \
         --password='${var.db_password}' \
-        '${aws_db_instance.mydb.name}' < '${path.module}/db/schema.sql'
+        '${aws_db_instance.mydb.db_name}' < '${path.module}/db/schema.sql'
     EOT
   }
 
