4e89db68c9d92b0b76a178db806b94da6ff9e571
pnaucke <nauckepieter@gmail.com>
2026-01-08 16:02:30 +0100

    new monitoring

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -4,7 +4,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
   dashboard_body = jsonencode({
     widgets = [
 
-# EC2 Instance metrics
+# EC2 Instance metrics - CPU
       {
         type = "metric"
         width = 12
@@ -26,6 +26,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       
+# EC2 Instance metrics - Network (gesplitst in 2 widgets)
       {
         type = "metric"
         width = 12
@@ -33,20 +34,36 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         properties = {
           metrics = [
             ["AWS/EC2", "NetworkIn", "InstanceId", aws_instance.web1.id, { stat = "Sum", label = "Web1 Network In" }],
-            ["AWS/EC2", "NetworkOut", "InstanceId", aws_instance.web1.id, { stat = "Sum", label = "Web1 Network Out" }],
+            ["AWS/EC2", "NetworkOut", "InstanceId", aws_instance.web1.id, { stat = "Sum", label = "Web1 Network Out" }]
+          ]
+          period = 60
+          stat = "Sum"
+          region = "eu-central-1"
+          title = "Web1 Network Traffic"
+          view = "timeSeries"
+          stacked = false
+        }
+      },
+      
+      {
+        type = "metric"
+        width = 12
+        height = 6
+        properties = {
+          metrics = [
             ["AWS/EC2", "NetworkIn", "InstanceId", aws_instance.web2.id, { stat = "Sum", label = "Web2 Network In" }],
             ["AWS/EC2", "NetworkOut", "InstanceId", aws_instance.web2.id, { stat = "Sum", label = "Web2 Network Out" }]
           ]
           period = 60
           stat = "Sum"
           region = "eu-central-1"
-          title = "WebServer Network Traffic"
+          title = "Web2 Network Traffic"
           view = "timeSeries"
           stacked = false
         }
       },
       
-# Nieuwe metric: Disk usage voor webservers
+# EC2 Instance metrics - Disk usage
       {
         type = "metric"
         width = 12
@@ -68,7 +85,7 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       
-# Database metrics - GECORRIGEERD: gebruik identifier in plaats van id
+# Database metrics - CPU
       {
         type = "metric"
         width = 12
@@ -89,33 +106,32 @@ resource "aws_cloudwatch_dashboard" "main_dashboard" {
         }
       },
       
+# Database metrics - Connections
       {
         type = "metric"
         width = 12
         height = 6
         properties = {
           metrics = [
-            ["AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", aws_db_instance.narre-db.identifier, { stat = "Average", label = "Database Connections" }],
-            # Toon beschikbare GB in plaats van bytes
-            [".", "FreeStorageSpace", ".", ".", { stat = "Average", label = "Free Storage (GB)", yAxis = "right", unit = "Gigabytes" }]
+            ["AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", aws_db_instance.narre-db.identifier, { stat = "Average", label = "Database Connections" }]
           ]
           period = 60
           stat = "Average"
           region = "eu-central-1"
-          title = "Database Performance"
-          view = "timeSeries"
+          title = "Database Connections"
+          view = "singleValue"
           stacked = false
         }
       },
       
-# Database vrije storage in GB
+# Database metrics - Free Storage in GB
       {
         type = "metric"
         width = 12
         height = 6
         properties = {
           metrics = [
-            ["AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", aws_db_instance.narre-db.identifier, { stat = "Average", label = "Free Storage", unit = "Gigabytes" }]
+            ["AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", aws_db_instance.narre-db.identifier, { stat = "Average", label = "Free Storage (GB)", unit = "Gigabytes" }]
           ]
           period = 60
           stat = "Average"
